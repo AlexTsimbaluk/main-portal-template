363afc7103a1926b142a34853b8db1d2f52a813c
Alexandr Tsimbaluk <aleksandr.tsimbaluk@gmail.com>
2018-03-22 20:26:54 +0300

    адаптивное меню в шапке, некоторая адаптация

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 $(document).on('ready', function() {
 	'use strict';
 	
+	$('.toggle-nav').on('click', function(e) {
+		// $('.header .nav-header').slideToggle(100);
+		if(!$('body').hasClass('menu-visible')) {
+			$('body').addClass('menu-visible');
+		}
+		else {
+			$('body').removeClass('menu-visible');
+		}
+	});
 });
